507af0eb4844cd5bb951165e99db837b530ec036
kalmanb <kalman@movio.co>
2013-07-20 11:17:50 +1200

    Tidy up Build.scala - inc akka 2.2.0

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -6,54 +6,40 @@ object $name$ extends Build {
 
   override lazy val settings = super.settings ++ Seq(resolvers := Seq())
 
-  val akkaVersion = "2.2-M3"
-//  val sprayVersion = "1.1-20130110"
+  val akkaVersion = "2.2.0"
 
   val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.10.0"
-//  val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
-//  val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
-//  val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
-//  val sprayCan = "io.spray" % "spray-can" % sprayVersion
-//  val sprayRouting = "io.spray" % "spray-routing" % sprayVersion
-//  val sprayCaching = "io.spray" % "spray-caching" % sprayVersion
-//  val sprayTestKit = "io.spray" % "spray-testkit" % sprayVersion
-//
+  //val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+  //val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
+  //val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
+  //val slf4j = "org.slf4j" % "slf4j-api" % "1.7.5"
 
-  val slf4j = "org.slf4j" % "slf4j-api" % "1.7.5"
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.0.M6-SNAP16" % "test"
+  // Test Libs
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.0.M6-SNAP33" % "test"
   val junit = "junit" % "junit" % "4.11" % "test"
   val mockito = "org.mockito" % "mockito-all" % "1.9.5" % "test"
 
   val publishedScalaSettings = Seq(
-    scalaVersion := "2.10.1",
-    //scalaBinaryVersion <<= scalaVersion,
-    //libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
-    //libraryDependencies in config("macro") <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
+    scalaVersion := "2.10.2",
     resolvers ++= Seq(
-      Resolver.sonatypeRepo("snapshots"),
-      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
- //     "spray repo" at "http://repo.spray.io",
- //     "spray repo nightlies" at "http://nightlies.spray.io"
+      //Resolver.sonatypeRepo("snapshots"),
+      //"Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
      ),
 
     libraryDependencies ++= Seq(
-      //scalaReflect,
-//      akkaActor,
-//      akkaSlf4j,
-//      akkaTestKit,
-//      sprayCan,
-//      sprayRouting,
-//      sprayCaching,
-//      slick,
-//
-//      // Testing Libs
-//      sprayTestKit,
-      slf4j,
+      //slf4j,
+      //akkaActor,
+      //akkaSlf4j,
+      //akkaTestKit,
+
+      // Testing Libs
+      sprayTestKit,
       scalaTest,
       junit,
       mockito))
 
-  lazy val root = Project(id = projectName,
+  lazy val root = Project(
+    id = projectName,
     base = file("."),
     settings = Project.defaultSettings ++ publishedScalaSettings)
 
