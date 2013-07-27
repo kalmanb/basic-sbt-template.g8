deea207ab67cc6a8eb2789885239b50c7dde472a
kalmanb <kalman@movio.co>
2013-07-27 21:17:51 +1200

    Move files into project dir Allow better project and file naming

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -1,14 +1,14 @@
 import sbt._
 import Keys._
 
-object $name$ extends Build {
-  val projectName = "$name$"
+object $name;format="Camel"$ extends Build {
+  val projectName = "$name;format="lower"$"
 
   override lazy val settings = super.settings ++ Seq(resolvers := Seq())
 
   val akkaVersion = "2.2.0"
 
-  val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.10.0"
+  //val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.10.0"
   //val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
   //val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
   //val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
